9ade6d4c84ba8f68cde102a290b61aae12ca7d66
Kunleweb <booment11@gmail.com>
2025-07-16 00:35:15 +0100

    Global error handler

--- prac.js
+++ prac.js
@@ -41,8 +41,29 @@ app.use('/api/v1/users',  userRouter)
 app.use(express.static(`${__dirname}/starter/public`))
 
 app.all('*', (req,res,next)=>{
-    res.status(404).json({status: 'fail', message: `Cant find ${req.originalUrl} on tbis server!`})
+    // res.status(404).json({status: 'fail', message: `Cant find ${req.originalUrl} on tbis server!`})
+
+    const err = new Error(`Cant find ${req.originalUrl} on tbis server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+// Why have we passed err into next function
+    next(err)
+
 })
+
+
+
+app.use((err, req, res, next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+res.status(err.statusCode).json({status:err.status, message:err.message})
+
+})
+
+
+
+
 module.exports = app;
 
 
