238a29471619bb1436eb5049fb62f5508c360f15
kunleweb <booment11@gmail.com>
2025-08-14 11:53:23 +0100

    login form

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,22 +15,23 @@ exports.getOverview = catchAsync(async (req, res, next)=> {
     })
 
 
-exports.getTour = catchAsync( async(req,res)=>{
+exports.getTour = catchAsync( async(req,res, next)=>{
     // 1) get the data, for the requested tour (including reviews and guides)
        const tour = await Tour.findOne({slug:req.params.slug}).populate({
         path: 'reviews',
         fields: 'review rating user'
        }) 
-   
-        
-
     // 2) Build template
-
     // 3) render the data using data from 1
-
-
     res.status(200).render('tour', {title: 'The forest Hiker Tour', tour})
 })
 
+
+exports.loginPage = (req, res)=>{
+
+    // 1) respond with the login page temp; 
+    res.status(200).render('login', {title: 'Log into your account',})
+
+}
 
 
--- routers/viewRoutes.js
+++ routers/viewRoutes.js
@@ -4,6 +4,7 @@ const viewController = require('./../controllers/viewsController')
 
 router.get('/', viewController.getOverview)
 router.get('/tour/:slug', viewController.getTour)
+router.get('/login', viewController.loginPage)
 
 
 module.exports = router
